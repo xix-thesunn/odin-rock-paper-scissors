985f5c679f3acc66749bf7a2935f914f4c342252
XIX The Sunn <131435522+xix-thesunn@users.noreply.github.com>
2026-09-12 16:31:34 +0300

    Reworked to visual

--- game.js
+++ game.js
@@ -1,10 +1,24 @@
-const startButton = document.querySelector("button");
-startButton.addEventListener("click", () => {
-    startGame()
-})
+const choiseButtons = document.querySelector(".choiseButtons");
+const buttons = choiseButtons.querySelectorAll('button');
+buttons.forEach(function(button) { 
+    button.addEventListener("click", () => {
+        playRound(button.id, getComputerChoise())
+    });
+});
+
+const scoreboard = document.querySelector(".scoreboard");
+const commentator = document.querySelector(".commentator")
+
+const winnerAnnouncer = document.createElement('p');
+winnerAnnouncer.classList.add('winnerAnnouncer');
+
+const gameScoreLimit = 5;
 
 let humanScore = 0;
 let computerScore = 0;
+
+let humanScoreTable = document.querySelector("#humanScoreTable");
+let computerScoreTable = document.querySelector("#computerScoreTable");
 
 function getComputerChoise() {
     let computerChoise;
@@ -15,26 +29,54 @@ function getComputerChoise() {
     return(computerChoise);
 }
 
-function getHumanChoise(i) {
-    let humanChoise = prompt("Round: " + ++i + "\n\nYour score: " + humanScore + "\nCPU score: " + computerScore + "\n\nEnter your choise: ");
-    if (humanChoise === null) {
-        return null;
-    }
-    else {    
-        humanChoise = humanChoise.toLowerCase();
-
-        if (humanChoise === "rock" || humanChoise === "paper" || humanChoise == "scissors") {
-            return(humanChoise);
-        }
-        else {
-            alert("You сan pick only between ROCK, PAPER and SCISSORS. Try again.")
-            return("none");
-        }
-    }
+function disableButtons() {
+    buttons.forEach((button) => button.disabled = true);
 }
 
-function gameRoundAlert(cpuChoise, message) {
-    alert("CPU choose " + cpuChoise.toUpperCase() + ".\n\n" + message);
+function enableButtons() {
+    buttons.forEach((button) => button.disabled = false);
+}
+
+function announceTheWinnerAndReset(winner, isHumanWin) {
+    winnerAnnouncer.textContent = winner + " took " + gameScoreLimit + " rounds! " + winner + " win!";
+    if (isHumanWin === false) {
+        winnerAnnouncer.classList.remove("winState");
+        winnerAnnouncer.classList.add("failState");
+    } else {
+        winnerAnnouncer.classList.add("winState");
+        winnerAnnouncer.classList.remove("failState");
+    }
+    document.body.appendChild(winnerAnnouncer);
+    disableButtons();
+    setTimeout(() => {
+        document.body.removeChild(winnerAnnouncer);
+        humanScore = 0;
+        computerScore = 0;
+        enableButtons();
+        commentator.classList.remove("winState");
+        commentator.classList.remove("failState");
+        commentator.textContent = "Make your choise to start the game...";
+    }, 2000)
+    }
+    
+
+function updateScoreboard() {
+    humanScoreTable.textContent = "You: " + humanScore;
+    computerScoreTable.textContent = "CPU: " + computerScore;
+
+    if (humanScore >= gameScoreLimit) {
+        announceTheWinnerAndReset("You", true);
+    };
+
+    if (computerScore >= gameScoreLimit) {
+        announceTheWinnerAndReset("CPU", false);
+    }
+
+}
+
+
+function writeRoundResults(cpuChoise, message) {
+    commentator.textContent = "CPU choose " + cpuChoise.toUpperCase() + ".\n\n" + message;
 }
 
 function winnerAlert(message) {
@@ -47,39 +89,41 @@ function playRound(humanChoise, computerChoise) {
     let isHumanWin;
 
     if (humanChoise === computerChoise) {
-        gameRoundAlert(computerChoise, "PARRY!")
+        writeRoundResults(computerChoise, "PARRY!")
+        commentator.classList.remove("winState");
+        commentator.classList.remove("failState");
     }
 
     else {
         if (humanChoise === "rock") {
             if (computerChoise === "paper") {
-                gameRoundAlert(computerChoise, "YOU LOSE!")
+                writeRoundResults(computerChoise, "YOU LOSE!")
                 isHumanWin = false;
             }
             else {
-                gameRoundAlert(computerChoise, "YOU WIN!")
+                writeRoundResults(computerChoise, "YOU WIN!")
                 isHumanWin = true;
             }
         }
 
         if (humanChoise === "paper") {
             if (computerChoise === "scissors") {
-                gameRoundAlert(computerChoise, "YOU LOSE!")
+                writeRoundResults(computerChoise, "YOU LOSE!")
                 isHumanWin = false;
             }
             else {
-                gameRoundAlert(computerChoise, "YOU WIN!")
+                writeRoundResults(computerChoise, "YOU WIN!")
                 isHumanWin = true;
             }
         }
 
         if (humanChoise === "scissors") {
             if (computerChoise === "rock") {
-                gameRoundAlert(computerChoise, "YOU LOSE!")
+                writeRoundResults(computerChoise, "YOU LOSE!")
                 isHumanWin = false;
             }
             else {
-                gameRoundAlert(computerChoise, "YOU WIN!")
+                writeRoundResults(computerChoise, "YOU WIN!")
                 isHumanWin = true;
             }
         }
@@ -87,9 +131,14 @@ function playRound(humanChoise, computerChoise) {
 
     if (isHumanWin === true) {
         ++humanScore;
+        commentator.classList.add("winState");
+        commentator.classList.remove("failState");
     }
     if (isHumanWin === false) {
         ++computerScore;
+        commentator.classList.remove("winState");
+        commentator.classList.add("failState");
     }
 
+    updateScoreboard();
 }
